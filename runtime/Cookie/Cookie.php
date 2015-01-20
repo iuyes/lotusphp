f53d357268fff854e3770e2e39b721bfb5d6efbd
李云龙 <iuyes@qq.com>
2015-01-20 11:56:58 +0800

    Update Cookie.php

--- runtime/Cookie/Cookie.php
+++ runtime/Cookie/Cookie.php
@@ -3,12 +3,12 @@
  * cookie
  * @author Jianxiang Qin <TalkativeDoggy@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause New BSD License
- * @version svn:$Id: Cookie.php 964 2012-08-27 04:02:32Z zhao5908@gmail.com $
+ * @version svn:$Id: Cookie.php 964 2015-01-20 11:57:32 iuyes@qq.com $
  */
 
 /**
  * cookie
- * @author Jianxiang Qin <TalkativeDoggy@gmail.com> Yi Zhao <zhao5908@gmail.com>
+ * @author Jianxiang Qin <TalkativeDoggy@gmail.com> Yi Zhao <zhao5908@gmail.com> Alex Lee <iuyes@qq.com>
  * @category runtime
  * @package   Lotusphp\Cookie
  */
